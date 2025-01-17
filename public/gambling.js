e06a4eaf484ecd7f7d7c2f3ac479690cf3b19066
cobdev <cobdev3@proton.me>
2025-01-17 17:16:20 -0600

    formatting

--- public/gambling.js
+++ public/gambling.js
@@ -1,48 +1,53 @@
 let theCollegeFund = 100;
-let gambling = false
+let gambling = false;
 const usernameRegex = /[^a-zA-Z0-9_]/gm;
 
 window.onload = () => {
-    fetch("/reset").then((response) => {
-	return response.json();
-    }).then((data) => {
-	if (data.money) {
-	    updateMoney(data.money);
-	}
-    })
-    loadLeaderboard()
+    fetch("/reset")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.money) {
+                updateMoney(data.money);
+            }
+        });
+    loadLeaderboard();
     document.getElementById("bet").focus();
-}
+};
 
 document.getElementById("gamble").addEventListener("click", gamble);
 
 document.getElementById("bet").addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-	gamble();
+        gamble();
     }
-})
+});
 
 document.getElementById("cash-out").addEventListener("click", () => {
     const username = document.getElementById("username").value;
     if (username.length === 0 || usernameRegex.test(username)) {
         showMessage("enter an actually valid username pls");
-	return;
+        return;
     }
     if (username.length > 20) {
         showMessage("username is too long idriot");
-    	return;
+        return;
     }
-    fetch(`/submit?username=${username}`).then(response => {
-	return response.json();
-    }).then(data => {
-	if (data.message) {
-	    showMessage(data.message)
-	    updateMoney(0)
-	    loadLeaderboard()
-	}
-    }).catch(() => {
-    	showMessage("something went wrong")
-    })
+    fetch(`/submit?username=${username}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.message) {
+                showMessage(data.message);
+                updateMoney(0);
+                loadLeaderboard();
+            }
+        })
+        .catch(() => {
+            showMessage("something went wrong");
+        });
 });
 
 function gamble() {
@@ -63,8 +68,8 @@ function gamble() {
         showMessage("you cant bet in the negatives idiort");
         return;
     }
-    if (gambling) return
-    gambling = true
+    if (gambling) return;
+    gambling = true;
     showMessage("gambling...");
     fetch(`/gamble?bet=${bet}`)
         .then((response) => {
@@ -73,8 +78,12 @@ function gamble() {
         .then((data) => {
             if (data.money || data.money === 0) {
                 updateMoney(data.money);
-                showMessage(`you ${data.win ? "win" : "lost"} $${bet.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`);
-		gambling = false
+                showMessage(
+                    `you ${data.win ? "win" : "lost"} $${bet
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
+                );
+                gambling = false;
             }
         })
         .catch(() => {
@@ -83,19 +92,27 @@ function gamble() {
 }
 
 function loadLeaderboard() {
-    fetch("/leaderboard").then((response) => {
-	return response.json();
-    }).then((data) => {
-	const leaderboard = document.getElementById("leaderboard")
-	leaderboard.innerHTML = data.map(({ username, money }) => {
-	    return `<li>${username}: $${money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</li>`
-	}).join("")
-    })
+    fetch("/leaderboard")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            const leaderboard = document.getElementById("leaderboard");
+            leaderboard.innerHTML = data
+                .map(({ username, money }) => {
+                    return `<li>${username}: $${money
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</li>`;
+                })
+                .join("");
+        });
 }
 
 function updateMoney(money) {
-    theCollegeFund = money
-    document.getElementById("money").innerHTML = `you have $${money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+    theCollegeFund = money;
+    document.getElementById("money").innerHTML = `you have $${money
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 }
 
 function showMessage(message) {
